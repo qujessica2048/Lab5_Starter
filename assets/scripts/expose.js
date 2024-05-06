aed31fba0129aed9361ba05fc929cf8375f5dca2
qujessica2048 <qu.jessica2048@gmail.com>
2024-05-06 16:47:38 -0700

    Finish expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -10,51 +10,48 @@ function init() {
   document.getElementById("horn-select").addEventListener('change', function() {
       // get the selected value from dropdown
       var selectedHorn = this.value;
-      console.log("Selected horn:", selectedHorn);
 
       // make path to svg image
       var imagePath = "./assets/images/" + selectedHorn + ".svg";
       var audioPath = "./assets/audio/" + selectedHorn + ".mp3";
-      console.log("Image path:", imagePath);
 
       // update the src of the image
       hornImage.src = imagePath;
       hornAudio.src = audioPath;
   });
 
-  var audioIcon = document.querySelector("#volume-controls img");
+  var audioIcon = document.querySelector("#volume-controls img"); // select the audio icon
 
-  document.getElementById("voloume-controls").addEventListener('change', function() {
+  document.getElementById("volume").addEventListener('input', function() {
     // get the volume
     var volumeValue = parseInt(this.value);
-    this.volume = volumeValue;
+    console.log(volumeValue);
 
+    // conditionals for icon
     if (volumeValue == 0) {
       audioIcon.src = "./assets/icons/volume-level-0.svg";
-    } else if (volumeValue >= 1 && volumeValue < 33 ) {
+    } else if (volumeValue < 33 ) {
       audioIcon.src = "./assets/icons/volume-level-1.svg";
-    } else if (volumeValue >= 33 && volumeValue < 67 ) {
+    } else if (volumeValue < 67 ) {
       audioIcon.src = "./assets/icons/volume-level-2.svg";
-    } else if (volumeValue >= 67) {
+    } else {
       audioIcon.src = "./assets/icons/volume-level-3.svg";
     } 
+
+    // make volume between 0 and 1
+    hornAudio.volume = volumeValue / 100;
+
   });
 
-  document.getElementById("button").addEventListener('click', function() {
-    // get the horn
-    var selectedHorn = hornSelect.value;
+  var playButton = document.querySelector("button");
 
-    // Set the audio source based on the selected horn
-    audioElement.src = `./assets/audio/${selectedHorn}.mp3`;
+  playButton.addEventListener('click', function() {
+    // play the audio
+    hornAudio.play();
 
-    // Set the volume level of the audio element
-    audioElement.volume = volumeRange.value / 100; // Convert range value to a decimal between 0 and 1
-
-    // Play the audio
-    audioElement.play();
-
-    // Trigger confetti using the js-confetti library
-    confetti();
+    // trigger confetti using the js-confetti library
+    const jsConfetti = new JSConfetti()
+    jsConfetti.addConfetti()
   });
 
-}
+} 
